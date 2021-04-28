ad677529bfc450f37cc7aa8817f7539c8443f8e6
bydmq <by_dmq@mail.ru>
2021-04-28 12:09:38 +0700

    push

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function longRange() {
+    var long=document.getElementById('long-range'); 
+    var longValue=document.getElementById('long-value'); 
+    longValue.innerHTML=long.value;
+
+}
+function heightRange() {
+    var height=document.getElementById('height-range');
+    var heightValue=document.getElementById('height-value'); 
+    heightValue.innerHTML=height.value;
+}
+function widthRange() {
+    var width=document.getElementById('width-range');
+    var widthValue=document.getElementById('width-value'); 
+    widthValue.innerHTML=width.value;
+}
